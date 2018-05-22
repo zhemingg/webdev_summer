ab765e73efd72c07299a2df2e273d783cb59226f
ZhemingGao <gao19911026@gmail.com>
2018-05-21 21:04:16 -0700

    created user admin stylesheet

---
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -0,0 +1,18 @@
+
+function User(username, password, firstName, lastName) {
+    this.username = username;
+    this.password = password;
+    // ...same for rest of properties…
+
+    this.setUsername = setUsername;
+    this.getUsername = getUsername;
+    // ...same for rest of properties…
+
+    function setUsername(username) {
+        this.username = username;
+    }
+    function getUsername() {
+        return this.username;
+    }
+    // ...same for rest of properties…
+}
