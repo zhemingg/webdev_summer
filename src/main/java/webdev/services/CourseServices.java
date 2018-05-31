2e9813282aed5eea3b8b6de8a007d8058fb5a43d
ZhemingGao <gao19911026@gmail.com>
2018-05-30 17:46:13 -0700

    change the content of the index.html

--- src/main/java/webdev/services/CourseServices.java
+++ src/main/java/webdev/services/CourseServices.java
@@ -1,5 +1,18 @@
 package webdev.services;
 
-public class CourseServices {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import webdev.models.Course;
+import webdev.repositories.CourseRepository;
+
+@RestController
+public class CourseServices {
+	@Autowired
+	CourseRepository courseRepository;	
+	@GetMapping("/api/course")
+	public Iterable<Course> findAllCourses() {
+		return courseRepository.findAll(); 
+	}
 }
