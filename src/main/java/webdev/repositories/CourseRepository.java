00a1aa7858af0f2dbbad6f3570ee62a8f99ffe03
ZhemingGao <gao19911026@gmail.com>
2018-05-30 17:33:18 -0700

    added course add repository

---
+++ src/main/java/webdev/repositories/CourseRepository.java
@@ -0,0 +1,8 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import webdev.models.Course;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+}
